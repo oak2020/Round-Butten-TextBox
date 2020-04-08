ec4a2e4c3e811bbdefee1949bb23733716944f8d
mohamad <mohamad13962018@gmail.com>
2020-04-08 14:44:10 +0430

    uopload game

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1586340786,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/character-sheet0.png",
+		"images/tiledbackground.png",
+		"images/value-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
